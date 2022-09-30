e6fec98af9d6f11b8781193d906aea871ea29543
moxeed <mortezabahjat@gmail.com>
2022-09-30 12:26:11 +0330

    first commit

---
+++ src/test/java/ir/moxeed/staff/StaffApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.moxeed.staff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
